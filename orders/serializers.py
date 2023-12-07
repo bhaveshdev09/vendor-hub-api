0d4d8e2490c59ec30dc2c3ebe933b3eda73fa8c2
Bhavesh Patil <patilbhavesh991209@gmail.com>
2023-12-07 13:38:47 +0530

    feat introduce for calculating vendor proformance matrix
    - add api endpoint for acknowledge the purchase order
    - add api endpoint for retreiving proformance maxtrix of vendor
    - instroduce signals to trigger the logical conditions

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,17 +1,17 @@
-from django.utils.translation import gettext as _
+import datetime
 from orders.models import PurchaseOrder
-from vendors.serializers import Vendor, serializers
+from orders.constants import OrderStatus
+from orders import signals
+from rest_framework import serializers
+from rest_framework.validators import ValidationError
+from django.utils.translation import gettext as _
+from django.utils import timezone
 
 
 class PurchaseOrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = PurchaseOrder
-        fields = "__all__"
-        read_only_fields = [
-            "id",
-            "created_at",
-            "updated_at",
-        ]
+        exclude = ["created_at", "updated_at"]
         extra_kwargs = {
             "po_number": {
                 "error_messages": {
@@ -27,5 +27,40 @@ class PurchaseOrderSerializer(serializers.ModelSerializer):
             "quantity": {
                 "error_messages": {"required": _("please provide total item quantity")}
             },
+            "quality_ratings": {"max_value": 10.00, "min_value": 0.0},
             "vendor": {"error_messages": {"required": _("please provide vendor Id")}},
         }
+
+    def validate(self, attrs):
+        return super().validate(attrs)
+
+    def update(self, instance, validated_data):
+        status = validated_data.get("status")
+        if instance.status == status == OrderStatus.COMPLETED:
+            raise ValidationError(
+                _("Purchase order status already marked as completed")
+            )
+        instance.status = status
+        instance.save()
+        if instance.status == OrderStatus.COMPLETED:
+            signals.purchase_order_status_completed.send(
+                sender=instance.__class__, instance=instance
+            )
+        return instance
+
+
+class PurchaseOrderAcknowledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseOrder
+        fields = ["acknowledgment_date"]
+
+    def update(self, instance, validated_data):
+        acknowledgment_date = validated_data.get("acknowledgment_date")
+        if not acknowledgment_date:
+            acknowledgment_date = timezone.now()
+        instance.acknowledgment_date = acknowledgment_date
+        instance.save()
+        signals.purchase_order_acknowledged.send(
+            sender=instance.__class__, instance=instance
+        )
+        return instance
--- vendors/views.py
+++ vendors/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from vendors.models import Vendor
-from vendors.serializers import VendorSerializer
+from vendors.models import Vendor, HistoricalPerformance
+from vendors.serializers import VendorSerializer, VendorPerformanceSerializer, HistoricalPerformanceSerializer
 
 
 class VendorListView(generics.ListCreateAPIView):
@@ -11,3 +11,14 @@ class VendorListView(generics.ListCreateAPIView):
 class VendorActionView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Vendor.objects.all()
     serializer_class = VendorSerializer
+
+
+class VendorPerformanceView(generics.RetrieveAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorPerformanceSerializer
+
+
+
+class VendorPerformanceHistoryView(generics.ListAPIView):
+    queryset = HistoricalPerformance.objects.all()
+    serializer_class = HistoricalPerformanceSerializer
